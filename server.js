04725682227dd87f1d0c02240eb4adb2620f75f3
root <root@MTthinkPad.>
2025-04-26 00:36:06 +1000

    still working on server

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 const postgres = require('postgres');
+
 const sql = postgres({
-    host: 'localhost',
-    port: 5432,
     username: 'postgres',
-    password: '',
     database: 'pwd_game'
 });
 const argon2 = require('argon2');
 require('dotenv').config();
 
+app.get('/public', (req, res) => {
+    res.send('/index.html');
+});
 
-
-
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
     console.log(`Your app is listening on port ${port}`);
 });
