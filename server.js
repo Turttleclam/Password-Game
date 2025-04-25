ed307b417a5f630650226d6a89113f68d9caeaf4
mark t <tomlinsonkram@gmail.com>
2025-04-25 15:03:46 +0000

    server

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const helmet = require('helmet');
-const postgres = require('postgres');
+const helmet = require("helmet");
+const postgres = require("postgres");
 
 const sql = postgres({
-    username: 'postgres',
-    database: 'pwd_game'
+  username: "postgres",
+  database: "pwd_game",
 });
-const argon2 = require('argon2');
-require('dotenv').config();
+const argon2 = require("argon2");
+require("dotenv").config();
 
-app.get('/public', (req, res) => {
-    res.send('/index.html');
+app.get("/", (req, res) => {
+  res.send("/index.html");
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Your app is listening on port ${port}`);
+  console.log(`Your app is listening on port ${port}`);
 });
