f7ac4ada5e928d424a2eba2ebd17d63590404543
root <root@MTthinkPad.>
2025-04-26 02:10:33 +1000

    got the server working, but it's not catching the css file

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
-const helmet = require("helmet");
-const postgres = require("postgres");
-=======
+
 const path = require('path');
 const helmet = require('helmet');
 const postgres = require('postgres');
->>>>>>> c862294 (Got the server, but it isn't picking up the css file)
 
 const sql = postgres({
   username: "postgres",
@@ -17,14 +13,10 @@ const argon2 = require("argon2");
 require("dotenv").config();
 
 // app.use(express.static('public'));
-<<<<<<< HEAD
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "public" + "index.html"));
-=======
+
 app.use('/style', express.static(`/style/styles.css`));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public' + '/index.html'));
->>>>>>> c862294 (Got the server, but it isn't picking up the css file)
 });
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname + '/style' + '/styles.css'));
